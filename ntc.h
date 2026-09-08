87902f55be783e951c06729a1b1aa888f18a01b3
Richard Vago <richard.vago@silabs.com>
2026-09-08 20:10:16 +0200

    enter measured beta value

--- ntc.h
+++ ntc.h
@@ -6,7 +6,7 @@
 // Divider: PD05 (drive high) - R4 - PC09 (IADC) - NTC - PC08 - R3(0R) - PD04 (drive low)
 #define NTC_R_SERIES_OHM  9940.0f   // R4
 #define NTC_R25_OHM       10000.0f
-#define NTC_BETA          3950.0f    // B25/85
+#define NTC_BETA          3050.0f    // B25/85
 
 // Returned by ntc_read_temp_c_x100() when the divider reads open or shorted.
 #define NTC_TEMP_INVALID  INT32_MIN
